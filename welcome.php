1be6d9cb4f21dbd10bba7ed9a79cdb8dbe505428
bltsandwich1 <itcaughtonj@gmail.com>
2014-10-09 20:50:13 -0500

    Break Fix ~~~ PHP test complete
    Pimpin

--- welcome.php
+++ welcome.php
@@ -2,7 +2,7 @@
 <body>
 <h1>~~<?php echo $_POST["tip"];?></h1>
 <?php echo $_POST["name"]; ?> found<br>
-<?php echo $_POST["description"]; ?>
+<?php echo $_POST["description"]; ?> <br>
 at <?php echo $_POST["location"]; ?>
 
 </body>
