f94a570873227d57e8454fea27c69338f6532834
bltsandwich1 <itcaughtonj@gmail.com>
2014-10-09 20:39:40 -0500

    PHP trial

---
+++ welcome.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+<h1>~~<?php echo $_POST["tip"];?></h1>
+<?php echo $_POST["name"]; ?> found<br>
+<?php echo $_POST["description"]; ?>
+at <?php echo $_POST["location"]; ?>
+
+</body>
+</html>
