6588e70d51caeddb006de3aeb897d0b17814f5c9
bltsandwich1 <itcaughtonj@gmail.com>
2014-10-10 00:53:18 -0500

    MySQL
    Oh god this isn't going to work

--- welcome.php
+++ welcome.php
@@ -1,9 +1,23 @@
-<html>
-<body>
-<h1>~~<?php echo $_POST["tip"];?></h1>
-<?php echo $_POST["name"]; ?> found<br>
-<?php echo $_POST["description"]; ?> <br>
-at <?php echo $_POST["location"]; ?>
+<?php
+$con=mysqli_connect("http://104.131.3.156/","root","Sandman1","test");
+// Check connection
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
 
-</body>
-</html>
+// escape variables for security
+$name = mysqli_real_escape_string($con, $_POST['name']);
+$tip = mysqli_real_escape_string($con, $_POST['tip']);
+$description = mysqli_real_escape_string($con, $_POST['description']);
+$location = mysqli_real_escape_string($con, $_POST['locaiton']);
+
+$sql="INSERT INTO tips (username, tip, description, locaiton)
+VALUES ('$name', '$tip', '$description', '$location')";
+
+if (!mysqli_query($con,$sql)) {
+  die('Error: ' . mysqli_error($con));
+}
+echo "1 record added";
+
+mysqli_close($con);
+?>
