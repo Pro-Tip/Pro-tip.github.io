9e27f28be2e8d835880c2d2cf5b6646e747f0c15
bltsandwich1 <itcaughtonj@gmail.com>
2014-10-11 22:18:58 -0500

    maybe if I address the row?

--- thanks.php
+++ thanks.php
@@ -15,13 +15,17 @@ $type = mysqli_real_escape_string($con, $_POST['type']);
 $sql="INSERT INTO tips1 (username, tip, description, location, type)
 VALUES ('$name', '$tip', '$description', '$location', '$type')";
 
-$id = mysqli_real_query($con,"SELECT id FROM tips1 WHERE name='$name' AND tip='$tip' AND description='$description' AND location='$location' AND type='$type'");
+$id = mysqli_real_query($con,"SELECT * FROM tips1 WHERE name='$name' AND tip='$tip' AND description='$description' AND location='$location' AND type='$type'");
 
 if (!mysqli_query($con,$sql)) {
   die('Error: ' . mysqli_error($con));
 }
 echo "Thanks for the tip! <br>";
-echo "Share your tip with <a href=tips.php?id=".$row['id'].">this link</a>";
+while($row = mysqli_fetch_array($id)) {
+    echo "Share your tip with <a href=tips.php?id=".$row['id'].">this link</a>";
+
+}
+
 
 mysqli_close($con);
 ?>
