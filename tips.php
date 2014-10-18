4fbf3f40403afe183adc04815196c1fdde722bfc
bltsandwich1 <itcaughtonj@gmail.com>
2014-10-18 16:53:45 -0500

    HTML works or nah?

--- tips.php
+++ tips.php
@@ -4,12 +4,12 @@ $con=mysqli_connect("localhost","root","Sandman1","test");
 if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
-echo"<title>Pro-Tip</title>";
+
 $post_id = mysqli_real_escape_string($con, $_GET['id']);
 
 $result = mysqli_query($con,"SELECT * FROM tips1
 WHERE id='$post_id'");
-echo "<table border='0'>
+echo "<html><title>Pro-Tip</title><body><table border='0'>
 <tr>
 <th>Tip</th>
 <th>Description</th>
@@ -23,7 +23,7 @@ while($row = mysqli_fetch_array($result)) {
 }
 
 echo"<a href='https://twitter.com/share' class='twitter-share-button' data-text='Added a Tip on @Pro-TipApp' data-via='MohnJoosemiller'>Tweet</a>
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>";
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script></body></html>";
 
 mysqli_close($con);
 ?>
