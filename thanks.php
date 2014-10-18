9a07a76619dbc48aead1a78053bdb0e69bddafb3
bltsandwich1 <itcaughtonj@gmail.com>
2014-10-18 14:43:06 -0500

    Reformat

--- thanks.php
+++ thanks.php
@@ -22,7 +22,7 @@ if (!mysqli_query($con,$sql)) {
 }
 echo "Thanks for the tip! <br>";
 while($row = mysqli_fetch_array($result)) {
-    echo "Share your tip with <a href=tips.php?id=".$row['id'].">this link</a>";
+    echo "Share your tip with <a href=tips.php?id=".$row['uname'].">this link</a>";
 }
 
 
