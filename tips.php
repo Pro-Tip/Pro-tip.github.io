1693edee9df61d35bd1c706c95aa0f5cb7fac93c
bltsandwich1 <itcaughtonj@gmail.com>
2014-10-18 16:48:44 -0500

    Page head?

--- tips.php
+++ tips.php
@@ -4,7 +4,7 @@ $con=mysqli_connect("localhost","root","Sandman1","test");
 if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
-
+echo"<head>Pro-Tip</head>";
 $post_id = mysqli_real_escape_string($con, $_GET['id']);
 
 $result = mysqli_query($con,"SELECT * FROM tips1
@@ -20,9 +20,10 @@ while($row = mysqli_fetch_array($result)) {
   echo"<tr>";
   echo "<td>" . $row['tip'] . "</td>" . "<td>" . $row['description'] ."</td>" . "<td>" . $row['username'] . "</td>" . "<td>" . $row['location'] . "</td>";
   echo"</tr>";
-  echo"<a href='https://twitter.com/share' class='twitter-share-button' data-text='Added a Tip on @Pro-TipApp' data-via='MohnJoosemiller'>Tweet</a>
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>";
 }
+
+echo"<a href='https://twitter.com/share' class='twitter-share-button' data-text='Added a Tip on @Pro-TipApp' data-via='MohnJoosemiller'>Tweet</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>";
 
 mysqli_close($con);
 ?>
